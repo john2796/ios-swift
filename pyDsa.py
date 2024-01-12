42c0c8f178dd72e27aab789529bec23992fb418e
john2796 <jbmiranda22796@gmail.com>
2024-01-11 20:29:54 -0800

    75 cc #2

--- pyDsa.py
+++ pyDsa.py
@@ -16,5 +16,16 @@ class Solution:
                     return res
 
 
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * (len(nums)) # [1, 1, 1, 1]
 
+        for i in range(1, len(nums)): # i = 1, 2, 3 skip 0
+            res[i] = res[i - 1] * nums[i - 1]
+        postfix = 1
+        # print(res) [1, 1, 2, 6]
 
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
