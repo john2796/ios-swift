3910fe110634ce946b90d5bb97b6e5e575995eb4
john2796 <jbmiranda22796@gmail.com>
2024-01-11 20:53:02 -0800

    75 cc #3

--- pyDsa.py
+++ pyDsa.py
@@ -27,3 +27,28 @@ class Solution:
             res[i] *= postfix
             postfix *= nums[i]
         return res
+
+
+
+"""
+128. Longest Consecutive Sequence
+return the length of the longest conseccutive elments sequence.
+Example 1:
+
+Input: nums = [100,4,200,1,3,2]
+Output: 4
+Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+"""
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        numSet = set(nums)
+        longest = 0
+
+        for n in numSet:
+            # check if start of sequence
+            if (n - 1) not in numSet:
+                length = 1
+                while (n + length) in numSet:
+                    length += 1
+                longest = max(longest, length)
+        return longest
