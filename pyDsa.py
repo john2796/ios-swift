cf1de168a87061ba212816dbbad7d200a7b549ae
john2796 <jbmiranda22796@gmail.com>
2024-01-14 09:15:17 -0800

    minWindow

--- pyDsa.py
+++ pyDsa.py
@@ -203,6 +203,51 @@ class Solution:
         return r - l + 1  # how did i get access to r from here??
 
 
+"""
+Example 1:
+
+Input: s = "ADOBECODEBANC", t = "ABC"
+Output: "BANC"
+Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
+# sliding window + hashset
+"""
+
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if t == "":
+            return ""
+        countT, window = {}, {}
+        # store t count in hashset
+        for c in t:
+            countT[c] = 1 + countT.get(c, 0)
+
+        have, need = 0, len(countT)
+        res, resLen = [-1, -1], float("infinity")
+
+        l = 0
+        for r in range(len(s)):
+            # expand window
+            c = s[r]
+            window[c] = 1 + window.get(c, 0)
+
+            if c in countT and window[c] == countT[c]:
+                have += 1
+            # shrink window
+            while have == need:
+                # update our result
+                if (r - l + 1) < resLen:
+                    res = [l, r]
+                    resLen = r - l + 1
+                # pop from left of our window
+                window[s[l]] -= 1
+                if s[l] in countT and window[s[l]] < countT[s[l]]:
+                    have -= 1
+                l += 1
+        l, r = res
+        return s[l : r + 1] if resLen != float("infinity") else ""
+
+
 # ----------------- Stack -----------------
 # ----------------- Binary Search -----------------
 # ----------------- Linked List -----------------
