d6a44127e175e2bf21a403463878acb3b6432b1a
john2796 <jbmiranda22796@gmail.com>
2024-01-15 17:22:24 -0800

    reorderList

--- pyDsa.py
+++ pyDsa.py
@@ -375,6 +375,43 @@ class Solution:
         return dummy.next
 
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        # find middle
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        # print(slow.val , fast.val) # slow == 2, fast ==4
+
+        # reverse second half
+        second = slow.next
+        prev = slow.next = None
+        # print(second.val, prev) # 3 None
+        while second:
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
+        # print(prev.val) # 4
+
+        # merge two halfs
+        first, second = head, prev  # 1, 4
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first, second = tmp1, tmp2
+
+
 # ----------------- Trees -----------------
 # ----------------- Tries -----------------
 # ----------------- Heap/Priority Queue -----------------
