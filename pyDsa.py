36bb3f82084009a6f7c50dec5e53c4d0afa2c3e4
john2796 <jbmiranda22796@gmail.com>
2024-01-15 17:05:58 -0800

    mergeLLS

--- pyDsa.py
+++ pyDsa.py
@@ -335,6 +335,46 @@ class Solution:
 
 
 # ----------------- Linked List -----------------
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, curr = None, head
+
+        while curr:
+            temp = curr.next  # store original curr
+            curr.next = prev  # set curr next to prev
+            prev = curr  # prev to next swap
+            curr = temp  # grab original temp set to curr
+        return prev
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(
+        self, list1: Optional[ListNode], list2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+        dummy = node = ListNode()
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                node.next = list1
+                list1 = list1.next
+            else:
+                node.next = list2
+                list2 = list2.next
+            node = node.next
+        node.next = list1 or list2
+        return dummy.next
+
+
 # ----------------- Trees -----------------
 # ----------------- Tries -----------------
 # ----------------- Heap/Priority Queue -----------------
