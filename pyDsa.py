25f051ab0fec0a30d0e877a2e78079b2a6cc60b5
john2796 <jbmiranda22796@gmail.com>
2024-01-16 18:07:28 -0800

    levelOrder

--- pyDsa.py
+++ pyDsa.py
@@ -585,6 +585,32 @@ class Solution:
                 return root
 
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        q = deque()
+        if root:
+            q.append(root)
+        res = []
+
+        while q:
+            val = []
+            for i in range(len(q)):
+                node = q.popleft()
+                val.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            res.append(val)
+        return res
+
+
 # ----------------- Tries -----------------
 # ----------------- Heap/Priority Queue -----------------
 # ----------------- Backtracking -----------------
