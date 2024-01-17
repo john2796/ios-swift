dc8a5a8797433c72ba702545337813b2d6c38efe
john2796 <jbmiranda22796@gmail.com>
2024-01-16 17:30:22 -0800

    lowestCommonAncestor

--- pyDsa.py
+++ pyDsa.py
@@ -561,6 +561,30 @@ class Solution:
         return False
 
 
+"""
+Given a binary search tree (BST), find the lowest common ancestor (LCA) node of two given nodes in the BST.
+
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+Output: 6
+Explanation: The LCA of nodes 2 and 8 is 6.
+"""
+
+
+class Solution:
+    def lowestCommonAncestor(
+        self, root: "TreeNode", p: "TreeNode", q: "TreeNode"
+    ) -> "TreeNode":
+        while True:
+            if root.val < p.val and root.val < q.val:
+                root = root.right
+            elif root.val > p.val and root.val > q.val:
+                root = root.left
+            else:
+                return root
+
+
 # ----------------- Tries -----------------
 # ----------------- Heap/Priority Queue -----------------
 # ----------------- Backtracking -----------------
