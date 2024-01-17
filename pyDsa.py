c3d78a82a2b48277c64629aa9b55b5301c1f8dd5
john2796 <jbmiranda22796@gmail.com>
2024-01-16 16:52:45 -0800

    maxDepth

--- pyDsa.py
+++ pyDsa.py
@@ -477,6 +477,47 @@ class Solution:
 
 
 # ----------------- Trees -----------------
+# Recursive DFS
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        left = 1 + self.maxDepth(root.left)
+        right = 1 + self.maxDepth(root.right)
+
+        return max(left, right)
+
+
+# Iterative DFS
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        stack = [[root, 1]]
+        res = 0
+        while stack:
+            node, depth = stack.pop()
+            if node:
+                res = max(res, depth)
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
+        return res
+
+# Iterative BFS
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        q = deque()
+        if root:
+            q.append(root)
+        level = 0
+
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            level += 1
+        return level
 # ----------------- Tries -----------------
 # ----------------- Heap/Priority Queue -----------------
 # ----------------- Backtracking -----------------
