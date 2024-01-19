25eaa416af8cfef5582fc4600c9ecbdf317ea1d2
john2796 <jbmiranda22796@gmail.com>
2024-01-18 17:29:28 -0800

    211. Design Add and Search Words Data Structure

--- pyDsa.py
+++ pyDsa.py
@@ -781,6 +781,16 @@ class TrieNode:
         self.end = False
 
 
+"""
+Implement the Trie class:
+
+- Trie() Initializes the trie object.
+- void insert(String word) Inserts the string word into the trie.
+- boolean search(String word) Returns true if the string word is in the trie (i.e., was inserted before), and false otherwise.
+- boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, and false otherwise.
+"""
+
+
 class Trie:
     def __init__(self):
         self.root = TrieNode()
@@ -816,6 +826,49 @@ class Trie:
         return True
 
 
+# same idea as Trie Class edge case it may contain '.' where dots can be mathed with any letter
+# use object instead of array of empty 26 alphabet
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.word = False
+
+
+class WordDictionary:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.word = True
+
+    def search(self, word: str) -> bool:
+        def dfs(j, root):
+            cur = root
+            for i in range(j, len(word)):
+                c = word[i]
+                if c == ".":
+                    for child in cur.children.values():
+                        if dfs(i + 1, child):
+                            return True
+                    return False
+                else:
+                    if c not in cur.children:
+                        return False
+                    cur = cur.children[c]
+            return cur.word
+
+        return dfs(0, self.root)
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
 # Your Trie object will be instantiated and called as such:
 # obj = Trie()
 # obj.insert(word)
